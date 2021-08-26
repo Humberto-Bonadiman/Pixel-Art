571404f47a732425b5f3098dd874a8850c7623cc
Humberto B. Bonadiman <humberto_bonadiman@hotmail.com>
2021-08-26 15:13:26 -0400

    Adição dos requisitos que faltavam

--- script.js
+++ script.js
@@ -1,11 +1,37 @@
 // 7.
 const blackColor = document.querySelector('.black');
-const yellowColor = document.querySelector('.yellow');
-const greenColor = document.querySelector('.green');
-const blueColor = document.querySelector('.blue');
-const selectedColor = document.querySelector('.selected');
-const selectPixel = document.querySelectorAll(".pixel");
+const secondColor = document.querySelector('.second');
+const thirdColor = document.querySelector('.third');
+const fourthColor = document.querySelector('.fourth');
+/* const selectedColor = document.querySelector('.selected');
+const selectPixel = document.querySelectorAll('.pixel'); */
 const pixelBoard = document.querySelector('#pixel-board');
+const boardSize = document.querySelector('#board-size');
+const generateBoard = document.querySelector('#generate-board');
+const getClearButton = document.querySelector('#clear-board');
+
+const color = () => Math.floor(Math.random() * 255);
+secondColor.style.backgroundColor = `rgb(${color()}, ${color()}, ${color()})`;
+thirdColor.style.backgroundColor = `rgb(${color()}, ${color()}, ${color()})`;
+fourthColor.style.backgroundColor = `rgb(${color()}, ${color()}, ${color()})`;
+
+function createPixelBoard(pixel) {
+  document.body.appendChild(pixelBoard);
+  for (let index = 1; index <= pixel; index += 1) {
+    const div = document.createElement('div');
+    pixelBoard.appendChild(div);
+    div.className = 'pixel-line';
+  }
+}
+
+function createPixels() {
+  const pixelLine = document.querySelectorAll('.pixel-line');
+  pixelLine.forEach((lines) => pixelLine.forEach(() => {
+    const pixel = document.createElement('span');
+    lines.appendChild(pixel);
+    pixel.className = 'pixel';
+  }));
+}
 
 function handleChangeColor(event) {
   const colorElement = document.querySelector('.selected');
@@ -14,42 +40,52 @@ function handleChangeColor(event) {
 }
 
 blackColor.addEventListener('click', handleChangeColor);
-yellowColor.addEventListener('click', handleChangeColor);
-greenColor.addEventListener('click', handleChangeColor);
-blueColor.addEventListener('click', handleChangeColor);
+secondColor.addEventListener('click', handleChangeColor);
+thirdColor.addEventListener('click', handleChangeColor);
+fourthColor.addEventListener('click', handleChangeColor);
 
 // 8.
 
-function setPixelColor (event) {
-  let selection = document.querySelector('.selected');
-  let selectionColor = window.getComputedStyle(selection).getPropertyValue("background-color");
-  event.target.style.backgroundColor = selectionColor
+function setPixelColor(event) {
+  const selection = document.querySelector('.selected');
+  const selectionColor = window.getComputedStyle(selection).getPropertyValue('background-color');
+  const element = event;
+  element.target.style.backgroundColor = selectionColor;
 }
-document.querySelector('#pixel-board').addEventListener('click', setPixelColor)
+document.querySelector('#pixel-board').addEventListener('click', setPixelColor);
 
 // 9.
-function buttonClear(buttonName) {
-  let buttonDad = document.querySelector("header");
-  let newButton = document.createElement("button");
-  let newButtonId = 'clear-board'
 
-  newButton.innerHTML = buttonName;
-  newButton.id = newButtonId;
-  buttonDad.appendChild(newButton);
-};
-
-buttonClear('Limpar');
-
-function clearPixels() {
-  let getClearButton = document.querySelector('#clear-board');
-  let getAllPixels = document.querySelectorAll('.pixel');
-  let setNewColor = 'white';
-
-  getClearButton.addEventListener('click', function() {
-    for (let index = 0; index < getAllPixels.length; index += 1) {
-      getAllPixels[index].style.backgroundColor = setNewColor;
-    }
-  })
+function clearPixel() {
+  const getAllPixels = document.querySelectorAll('.pixel');
+  const setNewColor = 'white';
+  for (let index = 0; index < getAllPixels.length; index += 1) {
+    getAllPixels[index].style.backgroundColor = setNewColor;
+  }
 }
 
-clearPixels();
+getClearButton.addEventListener('click', clearPixel);
+
+function changeSizePixelBoard() {
+  let getValue = boardSize.value;
+  console.log(getValue);
+  pixelBoard.innerText = '';
+  if (getValue.length > 0) {
+    if (getValue > 50) {
+      getValue = 50;
+    } else if (getValue < 5) {
+      getValue = 5;
+    }
+    createPixelBoard(getValue);
+    createPixels();
+  } else {
+    alert('Board inválido!');
+  }
+}
+
+generateBoard.addEventListener('click', changeSizePixelBoard);
+
+window.onload = () => {
+  createPixelBoard(5);
+  createPixels();
+};
